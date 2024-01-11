fdf6e38b544923a2df6fc02f2a444b687a916856
Dorian-MB <dorian.mariani@gmail.com>
2024-01-11 16:10:14 +0100

    update mapping

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -11,11 +11,13 @@ M.general_custom = {
         -- foo
         -- bar
     },
+
     n = {
         ["<leader>R"] = {":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gc<Left><Left><Left>", "Find and Replace current word"},
         ["<leader>rr"] = {":.,$s/\\<<C-r><C-w>\\>/<C-r><C-w>/gc<Left><Left><Left>", "Find and Replace current word from current position"},
         -- .,$s to apply it from the current line (denoted by .) to the end of the file (denoted by $).
     },
+
     v = {
         -- Move the selected line up or down (with correct indent)
         ["J"] ={":m '>+1<CR>gv=gv", "move down the selected line"},
