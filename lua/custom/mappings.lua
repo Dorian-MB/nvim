8f67a886da0a126168faee7ed525a9a8df1415fb
Dorian-MB <dorian.mariani@gmail.com>
2024-01-11 15:56:30 +0100

    re-organise mapping

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 
-M.general = {
+M.general_custom = {
     x = {
         -- by debault past over a selection doent copy
         -- but <leader> + p does
@@ -21,16 +21,17 @@ M.general = {
         ["J"] ={":m '>+1<CR>gv=gv", "move down the selected line"},
         ["K"] = {":m '<-2<CR>gv=gv", "move up the selected line"},
     },
+
     i = {
         -- Basic shortcut on most editor
-        ["<M-Left>"] = {'<C-o>b',opts = { noremap = true, silent = true }},
-        ["<M-Right>"] = {'<C-o>e<C-o>l',opts = { noremap = true, silent = true }},
-        ["<M-BS>"] = {'<M-BS>',opts = { noremap = true, silent = true }},
-        ["<M-Del>"] = {'<M-Del>',opts = { noremap = true, silent = true }},
-        ["<C-BS>"] = {'<Esc>d0x`[i',opts = { noremap = true, silent = true }},
-        ["<C-Del>"] = {'<Esc>D`[i',opts = { noremap = true, silent = true }},
+        ["<M-Left>"] = {'<C-o>b',opts = { noremap = true, silent = true }, "move cursor to the end of the word"},
+        ["<M-Right>"] = {'<C-o>e<C-o>l',opts = { noremap = true, silent = true }, "move cursor to the begening of the word"},
+        ["<M-BS>"] = {'<M-BS>',opts = { noremap = true, silent = true },  "delete to the begening of the word"},
+        ["<M-Del>"] = {'<M-Del>',opts = { noremap = true, silent = true }, "delete to the end of the word"},
+        ["<C-BS>"] = {'<Esc>d0x`[i',opts = { noremap = true, silent = true },  "delete to the begening of the line"},
+        ["<C-Del>"] = {'<Esc>D`[i',opts = { noremap = true, silent = true },   "delete to the end of the line"},
         -- Map Ctrl+Z to undo in insert mode --> MEGA : WARNING DONT DO IN NORMAL MODE LMAO
-        ["<C-z>"] = {'<C-o>u',opts = { noremap = true, silent = true }},
+        ["<C-z>"] = {'<C-o>u',opts = { noremap = true, silent = true }, "undo in insert mode, WARNING DONT DO IN NORMAL MODE"},
     }
 }
 
